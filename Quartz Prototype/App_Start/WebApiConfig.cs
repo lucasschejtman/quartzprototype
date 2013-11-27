a79c3ee47a54e5634baa4d3270d6abf0a8da2782
lucas-schejtman <lucas.schejtman@myob.com>
2013-11-28 11:20:53 +1300

    Added libraries

--- Quartz Prototype/App_Start/WebApiConfig.cs
+++ Quartz Prototype/App_Start/WebApiConfig.cs
@@ -15,7 +15,7 @@ namespace QuartzPrototype.App_Start
 
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
-            config.EnableSystemDiagnosticsTracing();
+            //config.EnableSystemDiagnosticsTracing();
         }
     }
 }
